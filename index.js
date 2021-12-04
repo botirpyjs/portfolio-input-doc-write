0083318521b4fc81f0fed8879c4364c8f8d05b97
Botir Akhmedov <95535127+botirpyjs@users.noreply.github.com>
2021-12-04 23:27:41 +0300

    portfolio-input-doc-write

---
+++ index.js
@@ -0,0 +1,11 @@
+/*qoliplar*/
+var messages = document.getElementById("messages");
+var textbox = document.getElementById("textbox");
+var button = document.getElementById("button");
+/*Bu yozilganda chiqarib beradigani */
+button.addEventListener("click", function () {
+  var newMessage = document.createElement("li");
+  newMessage.innerHTML = textbox.value;
+  messages.appendChild(newMessage);
+  textbox.value = "";
+});
